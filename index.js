d42c690bc9eb95c17f9f277617b71169f6d9cef1
Dorene07 <dstmarth@gmail.com>
2023-02-03 21:02:53 -0500

    messages array made in index server

--- index.js
+++ index.js
@@ -8,4 +8,33 @@ app.get('/', ( req, res )=>{
   app.get('/new', ( req, res )=>{   
     res.send('New Messages')
 });
+
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+ ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   app.listen(3000);
